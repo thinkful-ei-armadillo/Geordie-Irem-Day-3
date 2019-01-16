1d347be53aaf63534e0a7bdc4df2a42192b4b56f
Irem Sen <secilreel@yahoo.com>
2019-01-16 12:44:18 -0500

    findbyId

--- objectdrills.js
+++ objectdrills.js
@@ -77,3 +77,11 @@ function enrollInSummerSchool(students) {
     }
     return students;
 }
+
+}function findById(items, idNum) {
+  for (let i=0; i<items.length; i++){
+    if(items[i].id === idNum){
+      return items[i];
+    }
+  }
+}
