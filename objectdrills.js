4c1a2845036d31a76c0ae5e5b10df600d7e82f12
Irem Sen <secilreel@yahoo.com>
2019-01-16 12:01:55 -0500

    last bits of Drills1

--- objectdrills.js
+++ objectdrills.js
@@ -1,23 +1,46 @@
+'use strict'; 
 function createMyObject() {
-    const myObject = {
-        foo: 'bar',
-        answerToUniverse: 42,
-        olly_olly: 'oxen free',
-        hello: function() {
-            return 'hello!';
-        }
-    };
-    return myObject;
+  const myObject = {
+    foo: 'bar',
+    answerToUniverse: 42,
+    olly_olly: 'oxen free',
+    hello: function() {
+      return 'hello!';
+    }
+  };
+  return myObject;
 }
 
 function updateObject(obj) {
-    obj.foo = 'foo';
-    obj.bar = 'bar';
-    obj.bizz = 'bizz';
-    obj.bang = 'bang';
-    return obj;   
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
+  return obj;   
 }
 
-const myObj = createMyObject();
-console.log(myObj);
-console.log(updateObject(myObj));
+// const myObj = createMyObject();
+// console.log(myObj);
+// console.log(updateObject(myObj));
+
+function personMaker() {
+  const person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: function(){
+      return `${this.firstName} ${this.lastName}`;
+    },
+  };
+  return person;
+}
+// const person =personMaker();
+// console.log(person.fullName());
+
+function modifyKeyDeletor(obj){
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
+// console.log(modifyKeyDeletor(myObj));
