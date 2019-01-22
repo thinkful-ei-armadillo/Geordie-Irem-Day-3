5bd5dd67b4ab61a06716022189b7346ae53417d1
Geordie Connell <geordie.connell@gmail.com>
2019-01-22 17:45:47 -0500

    changes made in mentor session

--- objectdrills.js
+++ objectdrills.js
@@ -78,7 +78,7 @@ function enrollInSummerSchool(students) {
     return students;
 }
 
-}function findById(items, idNum) {
+function findById(items, idNum) {
   for (let i=0; i<items.length; i++){
     if(items[i].id === idNum){
       return items[i];
